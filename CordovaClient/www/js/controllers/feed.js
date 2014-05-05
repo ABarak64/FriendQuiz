8d6033d9ba8a5f4639ca61ab074391e3a46bdc97
Adam Barak <adam@pbhs.com>
2014-05-05 16:52:41 -0700

    Basic question and answer functionality working now.

--- CordovaClient/www/js/controllers/feed.js
+++ CordovaClient/www/js/controllers/feed.js
@@ -1,17 +1,40 @@
 angular.module('friendquiz')
 
     .controller('FeedCtrl', function ($scope, $stateParams, OpenFB, $ionicLoading, FriendQuizService) {
+        
+        $scope.answered = null;
+        $scope.question = null;
+        $scope.questionImg = 'img/mystery.jpg';
 
         $scope.show = function() {
             $scope.loading = $ionicLoading.show({
-                content: 'Loading feed...'
+                content: 'Getting a question...'
             });
         };
+
         $scope.hide = function(){
             $scope.loading.hide();
         };
 
-        function loadFeed() {
+        $scope.nextQuestion = function () {
+            $scope.answered = null;
+            $scope.question = null;
+            $scope.questionImg = 'img/mystery.jpg';
+            loadQuestion();
+        }
+
+        $scope.selectFriend = function (friend) {
+            $scope.questionImg = 'https://graph.facebook.com/' + $scope.question.mysteryStatus.from.id + '/picture';
+            var answered = {
+                message: 'Sorry, you were wrong.'
+            };
+            if (friend.id === $scope.question.mysteryStatus.from.id) {
+                answered.message = 'You were correct!';
+            }
+            $scope.answered = answered;
+        }
+
+        function loadQuestion() {
             $scope.show();
 
             FriendQuizService.getQuestion()
@@ -24,8 +47,6 @@ angular.module('friendquiz')
                 });
         }
 
-        $scope.doRefresh = loadFeed;
-
-        loadFeed();
+        loadQuestion();
 
     });
