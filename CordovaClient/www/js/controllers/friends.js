b75bba113926be059c917a8cbed4482861575bef
Adam Barak <adam@pbhs.com>
2014-06-12 16:13:42 -0700

    Giving the same pop-up protection on the high-score page.

--- CordovaClient/www/js/controllers/friends.js
+++ CordovaClient/www/js/controllers/friends.js
@@ -1,6 +1,11 @@
 angular.module('friendquiz')
 
     .controller('FriendsCtrl', function ($scope, $stateParams, FriendQuizService, $ionicLoading) {
+        // If getting friend scores causes an OAuth exception, hide the 'loading' screen if it is up.
+        $rootScope.$on('OAuthException', function () {
+            $scope.hide();
+        });
+
         $scope.loading = $ionicLoading.show({
             content: 'Getting Friend High Scores...'
         });
